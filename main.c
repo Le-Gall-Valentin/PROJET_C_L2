b07cb765214cc7afbdaf191a2d8b2a7a0f69d471
Le Gall <valentin.le-gall@efrei.net>
2023-11-13 10:08:20 +0100

    Add Excel file

--- main.c
+++ main.c
@@ -4,42 +4,32 @@
 #include "string.h"
 
 int main() {
-//    srand (time (NULL));
-//    for (int i = 7; i <= 20; ++i) {
-//        FILE *f = freopen("classic2.txt", "a", stdout);
-//        ListOfFloorCells *myList = createSortedListWithNValues(i);
-//        printf("%d; ", i);
-//
-//        startTimer();
-//        for (int j = 0; j < 10000; ++j) {
-//            int searchValue = rand();
-//            classicSearchValueInFloorList(myList, searchValue);
-//        }
-//        stopTimer();
-//        printf("%s\n", getTimeAsString());
-//        fclose(f);
-//
-//        FILE *f2 = freopen("levels2.txt", "a", stdout);
-//        printf("%d; ", i);
-//
-//        startTimer();
-//        for (int j = 0; j < 10000; ++j) {
-//            int searchValue = rand();
-//            levelSearchValueInFloorList(myList, searchValue);
-//        }stopTimer();
-//        stopTimer();
-//        printf("%s\n", getTimeAsString());
-//        fclose(f2);
-////        deleteFloorList(myList);
-//    }
-    ListOfFloorCells *myList = createEmptyFloorList(5);
-    addSortedCellInFloorList(myList,3,2);
-    addSortedCellInFloorList(myList,9,1);
-    addSortedCellInFloorList(myList,8,4);
-    addSortedCellInFloorList(myList,5,3);
-    addSortedCellInFloorList(myList,6,2);
-    addSortedCellInFloorList(myList,7,3);
-    addSortedCellInFloorList(myList,1,5);
-    displayFloorList(myList);
+    srand (time (NULL));
+    for (int i = 1; i <= 25; ++i) {
+        unsigned long long  searchValue = (unsigned long long)pow(2,i)-1;;
+        FILE *f = freopen("classic2.txt", "a", stdout);
+        ListOfFloorCells *myList = createSortedListWithNValues(i);
+        printf("%d; ", i);
+
+        startTimer();
+        for (int j = 0; j < 10000; ++j) {
+            classicSearchValueInFloorList(myList, searchValue);
+        }
+        stopTimer();
+        printf("%s\n", getTimeAsString());
+        fclose(f);
+
+        FILE *f2 = freopen("levels2.txt", "a", stdout);
+        printf("%d; ", i);
+
+        startTimer();
+        for (int j = 0; j < 10000; ++j) {
+            levelSearchValueInFloorList(myList, searchValue);
+        }stopTimer();
+        stopTimer();
+        printf("%s\n", getTimeAsString());
+        fclose(f2);
+//        deleteFloorList(myList);
+    }
     return 0;
 }
