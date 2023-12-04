cd2f690d4cdf2cd2e5e2852fbfb6c7b14297a8f3
Hugo Leyx-valade <113542060+Hugo-Leyx-valade@users.noreply.github.com>
2023-12-04 12:37:40 +0100

    Update Contact.c

--- Contact.c
+++ Contact.c
@@ -5,16 +5,19 @@
 #include "Contact.h"
 
 Contact *entryOfContact() {
+    // initialise le nom d'un contact
     char *name;
     Contact *contact;
     printf("insert lastname_firstname :");
     name = scanString();
+    //lance la fonction pour initialiser un contact
     contact = createContact(name);
     return contact;
 };
 
 
 Contact *createContact(char *name) {
+    
     for (int i = 0; i < strlen(name); ++i) {
         if (name[i] == ' ') {
             name[i] = '_';
@@ -23,6 +26,7 @@ Contact *createContact(char *name) {
     Contact *newContact = (Contact *) malloc(sizeof(Contact));
     newContact->lastnameFirstname = (char *) malloc(MAX_BUFFER * sizeof(char));
     strcpy(newContact->lastnameFirstname, name);
+    //met le nom en minuscule
     toLowerCase(newContact->lastnameFirstname);
     newContact->listOfEvent = NULL;
     return newContact;
@@ -30,11 +34,13 @@ Contact *createContact(char *name) {
 
 
 void displayContact(Contact *contact) {
+    // affiche le contact
     printf("%s \n", contact->lastnameFirstname);
     displayListOfEvents(contact->listOfEvent);
 }
 
 void addEventToContact(Contact *contact) {
+    // ajoute un event à la liste chainé d'évènnement 
     Event *newEventToAdd = createEvent();
     if (contact->listOfEvent == NULL) {
         contact->listOfEvent = newEventToAdd;
