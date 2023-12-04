b2753a73297239b95e9298f80d1b00f4ab9e1405
Hugo Leyx-valade <113542060+Hugo-Leyx-valade@users.noreply.github.com>
2023-12-04 13:31:18 +0100

    Update floorCell.c

--- floorCell.c
+++ floorCell.c
@@ -7,6 +7,7 @@
 
 
 FloorCell *createFloorCell(Contact *contact, int nbFloors) {
+    // Initialise une new cell avec les valeurs données en parramètre
     FloorCell *newFloorCell = (FloorCell *) malloc(sizeof(FloorCell));
     newFloorCell->value = contact;
     newFloorCell->nbFloors = nbFloors;
@@ -15,6 +16,7 @@ FloorCell *createFloorCell(Contact *contact, int nbFloors) {
 }
 
 FloorCell **createArrayOfNexts(int nbFloors) {
+    // initialise le tableau de next à NULL d'une taille nbFloor
     FloorCell **ArrayOfNext = (FloorCell **) malloc(nbFloors * sizeof(FloorCell *));
     for (int i = 0; i < nbFloors; i++) {
         ArrayOfNext[i] = NULL;
@@ -23,6 +25,7 @@ FloorCell **createArrayOfNexts(int nbFloors) {
 }
 
 int isEmptyArrayOfFloorCell(FloorCell **ArrayOfFloorCell, int size) {
+    // Test toutes les cases dutableau vérifiant si elles sont nulles
     for (int i = 0; i < size; i++) {
         if (ArrayOfFloorCell[i] != NULL) {
             return 0;
@@ -32,15 +35,18 @@ int isEmptyArrayOfFloorCell(FloorCell **ArrayOfFloorCell, int size) {
 }
 
 void deleteFloorCell(FloorCell *floorCell) {
+    //Supprime une FloorCell
     free(floorCell->arrayOfNexts);
     free(floorCell);
 }
 
 void displayFloorCellName(FloorCell *floorCell) {
+    // Affiche le nom d'une FloorCell
     printf("[ %s|@ ]", floorCell->value->lastnameFirstname);
 }
 
 void displayFloorCellAllInformations(FloorCell *floorCell) {
+    // Affiche toutes les infos d'un contact
     if (floorCell != NULL) {
         displayContact(floorCell->value);
     }
